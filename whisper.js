fcbcf94e2d2c1268ed71eb24ad42c06540c1dee1
arnorhs <arnorhs@gmail.com>
2013-03-20 22:51:40 -0700

    dropped requiring .forEach and improved error handling 1%

--- whisper.js
+++ whisper.js
@@ -4,7 +4,7 @@
 
   function listen(name, func) {
     if (!name || !func) {
-      throw new Error("To listen, you must provide an event name and a callback");
+      throw new Error("Whisper.listen: You must provide an event name and a callback");
     }
     if (!listeners[name]) {
       listeners[name] = [];
@@ -13,15 +13,20 @@
   }
 
   function say(name) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    if (!listeners[name]) {
-      return;
+    if (!name) {
+      throw new Error("Whisper.say: You must provide an event name");
     }
-    listeners[name].forEach(function(func) {
-      setTimeout(function() {
-        func.apply(window, args);
-      },0);
-    });
+    var arr = listeners[name],
+        args = Array.prototype.slice.call(arguments, 1);
+    if (!arr) return;
+
+    for (var i = 0, l = arr.length; i < l; i++) {
+      (function(func) {
+        setTimeout(function() {
+          func.apply(window, args);
+        },0);
+      })(arr[i]);
+    }
   }
 
   window.Whisper = {
